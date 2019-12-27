0316f310ffa881e30b84522c06a60557e3ad3018
kartikcode <59219455+kartikcode@users.noreply.github.com>
2019-12-27 10:44:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+
+class AddLocationForm(BoxLayout):
+      def search_location(self):
+            print("Explicit is better than implicit.")
+
 class WeatherApp(App):
       pass
+
 if __name__=='__main__':
          WeatherApp().run()
 
