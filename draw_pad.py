70d723c50da10c5f65727cb4025b940f1622487d
kartikcode <59219455+kartikcode@users.noreply.github.com>
2019-12-28 23:24:22 +0530

    Create draw_pad.py

---
+++ draw_pad.py
@@ -0,0 +1,26 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Line
+
+class DrawInput(Widget):
+    
+    def on_touch_down(self, touch):
+        print(touch)
+        with self.canvas:
+            touch.ud["line"] = Line(points=(touch.x, touch.y))
+        
+    def on_touch_move(self, touch):
+        print(touch)
+        touch.ud["line"].points += (touch.x, touch.y)
+		
+    def on_touch_up(self, touch):
+        print("RELEASED!",touch)
+
+
+class Drawing(App):
+    
+    def build(self):
+        return DrawInput()
+
+if __name__ == "__main__":
+    Drawing().run()
