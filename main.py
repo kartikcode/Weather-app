08859132ad3e69bd8bdf4be202417f998969c4fd
kartikcode <59219455+kartikcode@users.noreply.github.com>
2019-12-28 15:57:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import ObjectProperty
+from kivy.network.urlrequest import UrlRequest
+from kivy.uix.listview import ListItemButton
+
+class WeatherRoot(BoxLayout):
+      pass
 
 class AddLocationForm(BoxLayout):
+      search_input=ObjectProperty()
       def search_location(self):
-            print("Explicit is better than implicit.")
+          search_template="hhtps://api.openweathermap.org/data/2.5/" + "find?q={}&type=like"
+          search_url=search_template.format(self.search_input.text)
+          request=UrlRequest(search_url,self.found_location)
 
+      def found_location(self,request,data):
+          cities=["{}({})".format(d['name'],d['sys']['country'])
+                for d in data['list']]
+          self.search_results.item_strings=cities
+          self.search_results.adapter.data.clear()
+          self.search_results.adapter.data.extend(cities)
+          self.search_results._trigger_reset_populate()
+
+class LocationButton(ListItemButton):
+      pass       
+      
+      
 class WeatherApp(App):
       pass
 
